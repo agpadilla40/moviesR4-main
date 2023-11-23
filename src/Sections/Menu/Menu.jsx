fcf6e4a42a15c5b20676a9b899a354105b3840f6
agpadilla40 <agpadilla40@gmail.com>
2023-11-23 15:45:11 -0600

    corrección Menu.jsx

--- src/Sections/Menu/Menu.jsx
+++ src/Sections/Menu/Menu.jsx
@@ -4,7 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink } from "react-router-dom";
-import './menu.css';
+import './Menu.css';
 
 export default function Menu() {
     return (
